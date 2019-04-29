5f36fe453b95ae888a6dd818b4db548e8d90b552
Jorge Vargas <yorch.veintemil+github@gmail.com>
2019-04-29 00:37:11 -0700

    add dropdown to select analysis type

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import MenuItem from '@material-ui/core/MenuItem'
+import Select from '@material-ui/core/Select'
+
 import Decision from './Decision';
 
 import data from './labs.json'
+
+import departments from './departments.json'
 
 class App extends React.Component {
 
@@ -15,18 +20,37 @@ class App extends React.Component {
     this.setState({
       ...this.state,
       labs: data,
-      isLoading: false
+      isLoading: false,
+      departments: departments,
+      selectedDepartment: null,
+    })
+  }
+
+  selectDepartment = event => {
+    this.setState({
+      ...this.state,
+      selectedDepartment: event.target.value,
     })
   }
 
   render() {
-    const { labs, isLoading } = this.state
+    const { labs, isLoading, departments, selectedDepartment } = this.state
     if (isLoading) {
       return <p>Loading...</p>
     }
+    if (selectedDepartment === null) {
+      return (
+        <div className="App">
+          <p>Please select the type of clinical analysis you need:</p>
+          <Select value="" onChange={this.selectDepartment}>
+            {departments.map(d => <MenuItem key={d.id} value={d.name}>{d.name}</MenuItem>)}
+          </Select>
+        </div>
+      )
+    }
     return (
       <div className="App">
-        <Decision labs={labs.slice(0, 8)} />
+        <Decision labs={labs.filter(l => l.department === selectedDepartment).slice(0, 8)} />
       </div>
     );
   }
